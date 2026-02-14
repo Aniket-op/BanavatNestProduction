b4b205b0d4c494a1d4b7f12bdd8b9a04f5944edd
Aniket-op <ak.aniket807@gmail.com>
2026-02-14 16:59:14 +0530

    updated logo properties

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo2.jpg';
 
 interface LogoProps {
     className?: string;
     white?: boolean; // Option for white version if needed later
 }
 
-const Logo: React.FC<LogoProps> = ({ className = "h-20" }) => {
+const Logo: React.FC<LogoProps> = ({ className = "h-16" }) => {
     return (
         <img
             src={logo}
