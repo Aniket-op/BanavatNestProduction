6aa32c75c6800240e4807aca20f59f633ba92e1c
Aniket-op <ak.aniket807@gmail.com>
2026-02-15 14:59:15 +0530

    finialzed

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -55,7 +55,7 @@ const Footer = () => {
                     </div>
 
                     <div>
-                        <h4 className="text-zinc-900 dark:text-zinc-100 font-bold mb-8 text-lg">Contact & Address</h4>
+                        <h4 className="text-zinc-900 dark:text-zinc-100 font-bold mb-8 text-lg">Contact </h4>
                         <ul className="space-y-4 text-sm text-gray-500 dark:text-zinc-400 font-bold">
                             <li className="flex items-start"><Mail className="w-5 h-5 mr-3 text-[#84CC16] shrink-0" /> <a
                                 href="https://mail.google.com/mail/?view=cm&fs=1&to=info@banavatnest.com&su=Inquiry from Website&body=Hi BanavatNest Team,"
@@ -65,8 +65,8 @@ const Footer = () => {
                             >
                                 info@banavatnest.com
                             </a></li>
-                            {/* <li className="flex items-start"><Phone className="w-5 h-5 mr-3 text-[#84CC16] shrink-0" /> +91 99340 44777<br />+91 80023 96506</li>
-                            <li className="flex items-start"><MapPin className="w-5 h-5 mr-3 text-[#84CC16] shrink-0" />
+                            <li className="flex items-start"><Phone className="w-5 h-5 mr-3 text-[#84CC16] shrink-0" /> +91 99340 44777<br />+91 80023 96506</li>
+                            {/* <li className="flex items-start"><MapPin className="w-5 h-5 mr-3 text-[#84CC16] shrink-0" />
                                 <span>
                                     Registered Address:<br />
                                     C/O Utsab Roy,<br />
